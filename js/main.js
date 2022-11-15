4d99fd382e11dee8f9de30d97113fd449d533147
cynwebPD <113634821+cynwebPD@users.noreply.github.com>
2022-11-15 16:08:33 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+
+function modonocturno() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+
+    document.getElementById ("header").style.backgroundColor = "#341E1A";
+    document.getElementById ("header").style.color = "white";
+
+    document.getElementById ("main").style.backgroundColor = "black";
+    document.getElementById ("main").style.color = "white";
+
+
+    document.getElementById ("cardscolor").style.backgroundColor = "black";
+    document.getElementById ("cardscolor").style.borderColor= "white";
+    document.getElementById ("cardscolor2").style.backgroundColor = "black";
+    document.getElementById ("cardscolor2").style.borderColor= "white";
+    document.getElementById ("cardscolor3").style.backgroundColor = "black";
+    document.getElementById ("cardscolor3").style.borderColor= "white";
+    document.getElementById ("cardscolor4").style.backgroundColor = "black";
+    document.getElementById ("cardscolor4").style.borderColor= "white";
+}
+
+function copy() {
+    alert("Autora: Cynthia Sastre. EMAIL: cynthiasastre@gmail.com");
+    }
